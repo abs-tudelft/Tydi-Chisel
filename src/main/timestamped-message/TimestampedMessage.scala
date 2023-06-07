e3e24470e4bfb530e8b711c285e53223b4714d41
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-06-07 13:38:50 +0200

    Add timestamped message Chisel base code

---
+++ src/main/timestamped-message/TimestampedMessage.scala
@@ -0,0 +1,171 @@
+package main.timestamped_message
+
+import chisel3._
+import chisel3.util.{Cat, Decoupled, log2Ceil}
+import chisel3.experimental.dataview._
+import chisel3.internal.firrtl.Width
+import circt.stage.ChiselStage.{emitCHIRRTL, emitSystemVerilog}
+
+trait Element extends Bundle {
+  val elWidth: Int = 0
+  def getWidth: Int
+  def getElements: Seq[Data]
+//  val data: UInt = Wire(UInt(0.W))
+}
+
+class Null() extends Element {}
+
+class Group() extends Bundle with Element {
+//  def getWidth: Width = {
+//    width
+//  }
+}
+
+class Union() extends Element {
+//  def getWidth: Int = {
+//    elWidth
+//  }
+  val tag = UInt(0.W)
+  val value = UInt(0.W)
+
+//  def getElements: Seq[Data] = Seq[Data](UInt(elWidth.W))
+}
+class BitsEl(override val width: Width) extends Element {
+//  def getWidth: Int = {
+//    elWidth
+//  }
+
+  val value = Bits(width)
+
+//  def getElements: Seq[Data] = Seq[Data](UInt(elWidth.W))
+}
+
+class PhysicalStream(private val e: Element, val n: Int, val d: Int, val c: Int, private val u: Element) extends Bundle {
+  require(n >= 1)
+  require(1 <= c && c <= 7)
+
+  /** Indicates that the producer has valid data readyDu
+   *
+   * @group Signals
+   */
+  val valid = Output(Bool())
+
+  /** Indicates that the consumer is ready to accept the data this cycle
+   *
+   * @group Signals
+   */
+  val ready = Input(Bool())
+
+  private val indexWidth = log2Ceil(n)
+
+  val data = Output(UInt(e.getWidth.W))
+  // data := 50.U // Cannot set data here because data is not yet IO'ified
+//  e match {
+//    case e: Group => data := e.getElements.map(_.asUInt).reduce(Cat(_, _))
+//  }
+  val last = Output(UInt(d.W))
+  val stai = Output(UInt(indexWidth.W))
+  val endi = Output(UInt(indexWidth.W))
+  val strb = Output(UInt(n.W))
+}
+
+class TydiStream[T <: Element](val streamType: T = new Null,
+             val throughput: Double = 1.0, val dimensionality: Int = 0, val complexity: Int,
+             val user: Element = new Null, val keep: Boolean = false
+            ) extends RawModule {
+
+  val n: Int = throughput.ceil.toInt
+
+  val io: PhysicalStream = IO(new PhysicalStream(streamType, n=n, d=dimensionality, c=complexity, u=user))
+  def ioType: PhysicalStream = io.cloneType
+
+  val data: T = IO(Input(streamType))
+
+  val valid = IO(Input(Bool()))
+  io.valid := valid
+  val ready = IO(Output(Bool()))
+  ready := io.ready
+  val last = IO(Input(io.last.cloneType))
+  io.last := last
+  val stai = IO(Input(io.stai.cloneType))
+  io.stai := stai
+  val endi = IO(Input(io.endi.cloneType))
+  io.endi := endi
+  val strb = IO(Input(io.strb.cloneType))
+  io.strb := strb
+
+  // Auto concatenate all data elements
+  io.data := data.getElements.map(_.asUInt).reduce(Cat(_, _))
+}
+
+object TydiStream {
+  def apply[T <: Element](streamType: T = new Null,
+                          throughput: Double = 1.0, dimensionality: Int = 0, complexity: Int,
+                          user: Element = new Null, keep: Boolean = false): TydiStream[T] =
+    Module(new TydiStream(streamType, throughput, dimensionality, complexity, user, keep))
+}
+
+class TimestampedMessageBundle extends Group {
+  private val channelWidth: Width = 8.W
+  val time: UInt = UInt(64.W)
+  val message: UInt = UInt(channelWidth)
+}
+
+class TimestampedMessageModuleOut extends Module {
+  private val timestampedMessageBundle = new TimestampedMessageBundle // Can also be inline
+  // Create Tydi logical stream object
+  val stream: TydiStream[TimestampedMessageBundle] = TydiStream(timestampedMessageBundle, 1, complexity = 7)
+  // Create and connect physical stream following standard with concatenated data bitvector
+  val tydi_port: PhysicalStream = IO(stream.ioType)
+  tydi_port :<>= stream.io
+
+  // Assign values to logical stream group elements directly
+  stream.data.time := System.currentTimeMillis().U
+  stream.data.message := 0.U
+
+  // Assign some values to the other Tydi signals
+  // We have 1 lane in this case
+  stream.valid := true.B
+  stream.strb := 1.U
+  stream.stai := 0.U
+  stream.endi := 1.U
+  stream.last := 0.U
+}
+
+class TimestampedMessageModuleIn extends Module {
+  val io = IO(Flipped(new PhysicalStream(new TimestampedMessageBundle, n=1, d=2, c=7, u=new Null())))
+  io :<= DontCare
+  io.ready := DontCare
+}
+
+class TopLevelModule extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(64.W))
+    val out = Output(SInt(128.W))
+  })
+
+  val timestampedMessageOut = Module(new TimestampedMessageModuleOut())
+  val timestampedMessageIn = Module(new TimestampedMessageModuleIn())
+
+  // Bi-directional connection
+  timestampedMessageIn.io :<>= timestampedMessageOut.tydi_port
+  io.out := timestampedMessageOut.tydi_port.data.asSInt
+}
+
+object TimestampedMessage extends App {
+  private val firOpts: Array[String] = Array("-disable-opt", "-O=debug", "-disable-all-randomization", "-strip-debug-info"/*, "-preserve-values=all"*/)
+  println("Test123")
+
+  println(emitCHIRRTL(new TimestampedMessageModuleOut()))
+  println(emitSystemVerilog(new TimestampedMessageModuleOut(), firtoolOpts = firOpts))
+
+  println(emitCHIRRTL(new TimestampedMessageModuleIn()))
+  println(emitSystemVerilog(new TimestampedMessageModuleIn(), firtoolOpts = firOpts))
+
+  println(emitCHIRRTL(new TopLevelModule()))
+
+  // These lines generate the Verilog output
+  println(emitSystemVerilog(new TopLevelModule(), firtoolOpts = firOpts))
+
+  println("Done")
+}
