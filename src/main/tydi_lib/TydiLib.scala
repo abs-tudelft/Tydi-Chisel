a70e7522c26306effcf2dbc1aaaf461b49fc29a6
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-06-22 16:33:56 +0200

    Move base code over to lib.
    Add some docs.

--- src/main/tydi_lib/TydiLib.scala
+++ src/main/tydi_lib/TydiLib.scala
@@ -1,6 +1,7 @@
 package tydi_lib
 
 import chisel3._
+import chisel3.experimental.hierarchy.{Definition, Instance, instantiable, public}
 import chisel3.util.{Cat, PopCount, PriorityEncoder, log2Ceil}
 import chisel3.internal.firrtl.Width
 
@@ -269,4 +270,76 @@ class ComplexityConverter(val template: PhysicalStream, val memSize: Int) extend
   }
   out.stai := 0.U
 
+}
+
+/**
+ * Base definition for SubProcessor that only includes signal definitions used in [[MultiProcessorGeneral]]
+ */
+@instantiable
+abstract class SubProcessorSignalDef extends TydiModule {
+  // Declare streams
+  @public val out: PhysicalStream
+  @public val in: PhysicalStream
+}
+
+/**
+ * Basis of a SubProcessor definition that already includes the stream definitions and some base connections.
+ * @param e Element type to use for the streams
+ * @tparam T Element type of the stream
+ */
+@instantiable
+abstract class SubProcessorBase[T <: TydiEl](val e: T) extends SubProcessorSignalDef {
+  // Declare streams
+  val outStream: PhysicalStreamDetailed[T] = PhysicalStreamDetailed(e, n = 1, d = 0, c = 1, r = false)
+  val inStream: PhysicalStreamDetailed[T] = PhysicalStreamDetailed(e, n = 1, d = 0, c = 1, r = true)
+  val out: PhysicalStream = outStream.toPhysical
+  val in: PhysicalStream = inStream.toPhysical
+
+  // Connect streams
+  out :<>= in
+
+  // Set static signals
+  outStream.strb := 1.U
+  outStream.stai := 0.U
+  outStream.endi := 0.U
+  // stai and endi are 0-length
+}
+
+/**
+ * A MIMO processor that divides work over multiple sub-processors.
+ * @param e Element type to use for streams
+ * @param n Number of lanes/sub-processors
+ * @param processorDef Definition of sub-processor
+ */
+class MultiProcessorGeneral(val e: TydiEl, val processorDef: Definition[SubProcessorSignalDef], val n: Int = 6) extends TydiModule {
+  val out: PhysicalStream = IO(new PhysicalStream(e, n=n, d=0, c=7))
+  val in: PhysicalStream = IO(Flipped(new PhysicalStream(e, n=n, d=0, c=7)))
+
+  val elSize: Int = e.getWidth
+
+  out.valid := true.B
+  out.last := 0.U
+  out.stai := 0.U
+  out.endi := 0.U
+
+  private val subProcessors = for (i <- 0 until n) yield {
+    val processor: Instance[SubProcessorSignalDef] = Instance(processorDef)
+    //    val processor: SubProcessor = Module(new SubProcessor)
+    processor.in.strb := 1.U  // Static signal
+    processor.in.stai := 0.U  // Static signal
+    processor.in.endi := 0.U  // Static signal
+    processor.in.valid := in.strb(i)  // Sub input is valid when lane is valid
+    processor.in.last := DontCare
+    processor.in.data := in.data((elSize*i+1)-1, elSize*i)  // Set data
+    processor.out.ready := out.ready
+    processor
+  }
+
+  private val inputReadies = subProcessors.map(_.in.ready)
+  in.ready := inputReadies.reduce(_&&_)  // Top input is ready when all the modules are ready
+
+  // Top lane is valid when sub is ready Todo: factor in out ready here
+  out.strb := subProcessors.map(_.out.strb).reduce(Cat(_,_))
+  // Re-concat all processor output data
+  out.data := subProcessors.map(_.out.data).reduce(Cat(_, _))
 }
