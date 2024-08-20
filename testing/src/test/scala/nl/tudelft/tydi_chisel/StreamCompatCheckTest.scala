1900c32b32043fc017e7e759b40972efd3fe451b
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-08-20 16:57:35 +0200

    Fix uninitialized signal errors.

--- testing/src/test/scala/nl/tudelft/tydi_chisel/StreamCompatCheckTest.scala
+++ testing/src/test/scala/nl/tudelft/tydi_chisel/StreamCompatCheckTest.scala
@@ -15,27 +15,27 @@ class StreamCompatCheckTest extends AnyFlatSpec with ChiselScalatestTester {
   class MyBundle2 extends MyBundle
 
   class StreamConnectMod(in: PhysicalStream, out: PhysicalStream) extends TydiModule {
-    private val inStream = Wire(in)
-    private val outStream = Wire(out)
+    val inStream: PhysicalStream  = IO(Flipped(in))
+    val outStream: PhysicalStream = IO(out)
     outStream := inStream
-    out := in
   }
 
   class DetailedStreamConnectMod[TIel <: TydiEl, TIus <: Data, TOel <: TydiEl, TOus <: Data](
     in: PhysicalStreamDetailed[TIel, TIus],
     out: PhysicalStreamDetailed[TOel, TOus]
   ) extends TydiModule {
-    private val inStream = Wire(in)
-    private val outStream = Wire(out)
+    val inStream: PhysicalStreamDetailed[TIel, TIus]  = IO(Flipped(in)).flip
+    val outStream: PhysicalStreamDetailed[TOel, TOus] = IO(out)
     outStream := inStream
   }
 
-  private val myBundleStream = new PhysicalStreamDetailed(new MyBundle, c=8)
-  private val myBundle2Stream = new PhysicalStreamDetailed(new MyBundle2, c=8)
+  private val myBundleStream  = new PhysicalStreamDetailed(new MyBundle, c = 8)
+  private val myBundle2Stream = new PhysicalStreamDetailed(new MyBundle2, c = 8)
 
   behavior of "Stream compatibility check"
 
   it should "check type" in {
+    test(new DetailedStreamConnectMod(myBundleStream, myBundleStream)) { _ => }
     intercept[TydiStreamCompatException] {
       test(new DetailedStreamConnectMod(myBundleStream, myBundle2Stream)) { _ => }
     }
