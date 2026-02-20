0f17dd95f1ef3180dde1757b1bfb29f0186423c9
Casper Cromjongh <c.cromjongh@tudelft.nl>
2026-02-20 18:02:54 +0100

    Create stream driver and monitor using ChiselSim.

---
+++ testing/src/main/scala/nl/tudelft/tydi_chisel_test/NewStreamDriver.scala
@@ -0,0 +1,52 @@
+package nl.tudelft.tydi_chisel_test
+
+import chisel3._
+import nl.tudelft.tydi_chisel.{PhysicalStreamDetailed, TydiEl}
+import chisel3.simulator.PeekPokeAPI._
+
+class NewStreamDriver[Tel <: TydiEl, Tus <: Data](sink: PhysicalStreamDetailed[Tel, Tus]) {
+  private var remainingData = List.empty[(Seq[Option[Tel]], Option[Tus])]
+
+  def enqueueData(data: Seq[Seq[Tel]]): Unit = {
+    remainingData = data.map(el => (el.map(Some(_)), None)).toList
+  }
+
+  private val n = sink.n
+
+  def tick(): Unit = {
+    if (remainingData.nonEmpty) {
+      // If we have data to send, assert `valid` and write data to bus.
+      sink.valid.poke(true)
+      val dataPacket = remainingData.head._1
+
+      0 until n foreach { i =>
+        val lanePacket = if (i < dataPacket.length) dataPacket(i) else None
+        if (lanePacket.isDefined) {
+          sink.data(i).poke(lanePacket.get)
+          sink.strb(i).poke(true)
+        } else {
+          sink.data(i).poke(0)
+          sink.strb(i).poke(false)
+        }
+      }
+
+      val userPacket = remainingData.head._2
+      if (userPacket.isDefined) {
+        sink.user.poke(userPacket.get)
+      } else {
+        sink.user.poke(0)
+      }
+
+      // When the sink is also ready, we remove the element we sent.
+      if (sink.ready.peek().litToBoolean) {
+        remainingData = remainingData.tail
+      }
+    } else {
+      // If we run out of data, `valid` is de-asserted.
+      sink.valid.poke(false)
+    }
+  }
+
+  def isDone: Boolean = remainingData.isEmpty
+
+}
---
+++ testing/src/main/scala/nl/tudelft/tydi_chisel_test/NewStreamMonitor.scala
@@ -0,0 +1,36 @@
+package nl.tudelft.tydi_chisel_test
+
+import chisel3.Data
+import nl.tudelft.tydi_chisel.{PhysicalStreamDetailed, TydiEl}
+import chisel3.simulator.PeekPokeAPI._
+
+class NewStreamMonitor[Tel <: TydiEl, Tus <: Data](source: PhysicalStreamDetailed[Tel, Tus]) {
+  private val _received = scala.collection.mutable.ListBuffer[(Seq[Option[BigInt]], BigInt)]()
+
+  def received: Seq[(Seq[Option[BigInt]], BigInt)] = _received.toSeq
+
+  private val n = source.n
+
+  def tick(): Unit = {
+    // Always be ready to receive
+    source.ready.poke(true)
+
+    if (source.valid.peek().litToBoolean) {
+      val dataValues = 0 until n map { i =>
+        val stai = source.stai.peek().litValue
+        val endi = source.endi.peek().litValue
+        val strb = source.strb(i).peek().litToBoolean
+
+        // If the lane is valid, we read its value and put it in a `Some`
+        if (strb && i >= stai && i <= endi) {
+          Some(source.data(i).peekValue().asBigInt)
+        } else {
+          None
+        }
+      }
+      val userValue = source.user.peekValue().asBigInt
+      val rowValue: (Seq[Option[BigInt]], BigInt) = (dataValues, userValue)
+      _received += rowValue
+    }
+  }
+}
